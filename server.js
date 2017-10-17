fc8e859905f01322689e3b3e9257bcd27fbb7268
Vanessa Sena <nessa.aline@gmail.com>
2017-10-16 22:18:02 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dateformat = require('dateformat');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.get('/:time', (req, res) => {
+  var time = req.params.time;
+  var result = {};
+  if ( /^[0-9]*$/.test(time) ){
+    result.unix = time;
+    result.natural = dateformat(new Date(time*1000), 'mmmm dd, yyyy');
+  } else {
+    try {
+      var newDate = new Date(time);
+      result.unix = newDate.getTime()/1000;
+      result.natural = dateformat(newDate, 'mmmm dd, yyyy');
+    } catch (e) {
+      result.unix = null;
+      result.natural = null;
+    }
+  }
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}.`);
+});
